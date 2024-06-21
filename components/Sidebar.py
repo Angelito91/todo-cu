d4eb6867559e35178dbc1aa97ae45c8d25d08561
DaddyAsdrer <asdrubalacostamunoz@gmail.com>
2024-06-21 15:40:50 -0400

    Adding new components to the sidebar:  Three different buttons were added:  - One for categories  - Another one for settings  - And the last one to get out the app

--- components/Sidebar.py
+++ components/Sidebar.py
@@ -1,16 +1,15 @@
 from flet import *
 
 def Sidebar(page: Page):
+    category_button = TextButton(text = 'Categorias', icon = icons.LIST)
+    config_button = TextButton(text = 'Configuracion', icon = icons.SETTINGS)
+    exit_button = TextButton(text = 'Salir', icon = icons.EXIT_TO_APP)
     return Container(
-        content=Column([
-            Text(
-                "Hola, como estas?",
-                text_align= TextAlign.CENTER,
-                size=20,
-                weight=FontWeight.BOLD,
-                font_family='monospace',
-                italic=True)
-        ]),
+        content=Column(controls=[
+            category_button,
+            config_button,
+            exit_button
+            ]),
         height=page.window.height,
         width=page.window.width * 1 / 4,
         padding=padding.all(5),
