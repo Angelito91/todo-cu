17082cb2266b3a23537d72dc8cae088723e7d8f5
Angelito91 <portuondoangel@gmail.com>
2024-06-20 21:07:47 -0400

    add page config

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "App"
- 
+    page.title = "ToDo CU"
+    page.adaptive = True
+    page.theme = ft.Theme(color_scheme_seed="red")
+    page.window.width = 800
+    page.window.height = 600
+    page.scroll = "always"
+
     page.add(ft.Text("Hola"))
 
 
