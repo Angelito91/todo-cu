ff122da42f6838958c306373911fcb1b2aacbf3b
Angelito91 <portuondoangel@gmail.com>
2024-06-25 15:54:13 -0400

    changed code from func to class

--- components/Forms.py
+++ components/Forms.py
@@ -1,72 +1,79 @@
 from flet import *
 
-def Forms(page: Page):
 
-    def cancel(e):
-        title.value = ''
-        content.value = ''
-        page.update()
-        
-    title = TextField(
-        label='Titulo',
-        hint_text="Escriba un titulo... Ej: Primera nota",
-        prefix_icon= icons.ADD_TASK,
-        width=page.window.width * 2.86 / 4,
-        border=InputBorder.NONE,
-        fill_color=colors.with_opacity(0.05,'white'),
-        filled=True,
-        autocorrect=True,
-        autofocus=True
-    )
-    
-    content = TextField(
-        label='Contenido',
-        hint_text="Escriba de que trata la nota..",
-        prefix_icon= icons.BOOKMARKS_SHARP,
-        width=page.window.width * 2.86 / 4,
-        border=InputBorder.NONE,
-        fill_color=colors.with_opacity(0.05,'white'),
-        filled=True,
-        multiline=True,
-        autocorrect=True,
-        min_lines=4,
-        max_lines=6,
-        max_length=200
-    )
-    
-    button_add = ElevatedButton(
-        text='Guardar',
-        icon=icons.ADD,
-        color=colors.GREEN_ACCENT,
-        width=page.window.width * 1.50 / 4,
-        height=40
-    )
+class Forms(Column):
+    def __init__(self,page):
+        super().__init__()
 
+        self.page = page
 
-    button_cancel = ElevatedButton(
-        text='Descartar',
-        icon=icons.CANCEL,
-        color=colors.RED,
-        width=page.window.width * 1.25 / 4,
-        height=40,
-        on_click = cancel
-    )
+        self.col = 9
+        self.height=self.page.window.height,
+        self.width=self.page.window.width * 3 / 4,
 
-    return Column([
-        Text(
-            "En que piensas 🧠?",
-            text_align= TextAlign.CENTER,
-            size=20,
-            weight=FontWeight.BOLD,
-            font_family='monospace'),
-        title,
-        content,
-        Row([
-            button_add,
-            button_cancel
-        ])
-    ],
-    col=9,
-    height=page.window.height,
-    width=page.window.width * 3 / 4,
-    )
+        self.title = TextField(
+            label='Titulo',
+            hint_text="Escriba un titulo... Ej: Primera nota",
+            prefix_icon=icons.ADD_TASK,
+            width=self.page.window.width * 2.86 / 4,
+            border=InputBorder.NONE,
+            fill_color=colors.with_opacity(0.05, 'white'),
+            filled=True,
+            autocorrect=True,
+            autofocus=True
+        )
+
+        self.content = TextField(
+            label='Contenido',
+            hint_text="Escriba de que trata la nota..",
+            prefix_icon=icons.BOOKMARKS_SHARP,
+            width=self.page.window.width * 2.86 / 4,
+            border=InputBorder.NONE,
+            fill_color=colors.with_opacity(0.05, 'white'),
+            filled=True,
+            multiline=True,
+            autocorrect=True,
+            min_lines=4,
+            max_lines=6,
+            max_length=200
+        )
+
+        self.button_add = ElevatedButton(
+            text='Guardar',
+            icon=icons.ADD,
+            color=colors.GREEN_ACCENT,
+            width=self.page.window.width * 1.50 / 4,
+            height=40
+        )
+
+        self.button_cancel = ElevatedButton(
+            text='Descartar',
+            icon=icons.CANCEL,
+            color=colors.RED,
+            width=self.page.window.width * 1.25 / 4,
+            height=40,
+            on_click=self.cancel
+        )
+
+        self.controls = [
+            Text(
+                "En que piensas 🧠?",
+                text_align=TextAlign.CENTER,
+                size=20,
+                weight=FontWeight.BOLD,
+                font_family='monospace'),
+            self.title,
+            self.content,
+            Row([
+                self.button_add,
+                self.button_cancel
+            ])
+        ]
+
+    def cancel(self, e):
+        self.title.value = ''
+        self.content.value = ''
+        self.page.update()
+
+    def build(self):
+        return super().build()
--- components/Sidebar.py
+++ components/Sidebar.py
@@ -2,55 +2,63 @@ from flet import *
 from getpass import getuser
 
 
-def Sidebar(page: Page):
+class Sidebar(Container):
+    def __init__(self,page: Page):
+        super().__init__()
+        
+        self.page = page
+        
+        self.col = 3
+        self.height = self.page.window.height
+        self.padding = padding.symmetric(horizontal=5, vertical=8)
+        self.border = border.only(
+            right=border.BorderSide(0.5, colors.WHITE30))
+        self.bgcolor = colors.with_opacity(0.05, color='red')
 
-    def close(e):
-        page.window.close()
+        self.avatar = CircleAvatar(
+            foreground_image_src='https://avatars.githubusercontent.com/u/111069220',
+            width=100,
+            height=100
+        )
 
-    avatar = CircleAvatar(
-        foreground_image_src='https://avatars.githubusercontent.com/u/111069220',
-        width=100,
-        height=100
-    )
+        self.username = Text(
+            '👋 ' + getuser().capitalize(),
+            size=18,
+            weight=FontWeight.BOLD,
+        )
 
-    username = Text(
-        '👋 ' + getuser().capitalize(),
-        size=18,
-        weight=FontWeight.BOLD,
-    )
+        self.category_button = TextButton(
+            text='Categorias',
+            icon=icons.LIST
+        )
 
-    category_button = TextButton(
-        text='Categorias',
-        icon=icons.LIST
-    )
+        self.config_button = TextButton(
+            text='Configuracion',
+            icon=icons.SETTINGS,
+        )
 
-    config_button = TextButton(
-        text='Configuracion',
-        icon=icons.SETTINGS,
-    )
+        self.exit_button = TextButton(
+            text='Salir', icon=icons.EXIT_TO_APP, on_click=self.close)
 
-    exit_button = TextButton(
-        text='Salir', icon=icons.EXIT_TO_APP, on_click=close)
-
-    return Container(
-        ListView([
+        self.content = ListView([
             Column([
-                avatar,
-                username,
-            ],horizontal_alignment=CrossAxisAlignment.CENTER),
+                self.avatar,
+                self.username,
+            ], horizontal_alignment=CrossAxisAlignment.CENTER),
             Divider(color=colors.with_opacity(0.1, colors.WHITE30)),
             Column([
-                category_button,
+                self.category_button,
             ]),
             Divider(color=colors.with_opacity(0.1, colors.WHITE30)),
             Column([
-                config_button,
-                exit_button
+                self.config_button,
+                self.exit_button
             ])
-        ]),
-        col=3,
-        height=page.window.height,
-        padding=padding.symmetric(horizontal=5, vertical=8),
-        border=border.only(right=border.BorderSide(0.5, colors.WHITE30)),
-        bgcolor=colors.with_opacity(0.05, color='red')
-    )
+        ])
+
+    def close(self,e):
+        self.page.window.close()
+
+
+    def build(self):
+        return super().build()
